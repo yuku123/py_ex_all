8ebfb143f5a2ad96ed434570a3b5dbaf13fd60c6
张陈浩 <chenhao.zhang@2piday.com>
2020-12-14 13:55:22 +0800

    add code

--- case/pandas/aa.py
+++ case/pandas/aa.py
@@ -3,6 +3,8 @@ import numpy as np
 
 pd.set_option("display.max_columns",500)
 
-csv_data = pd.read_csv("../_data/StudentsPerformance.csv")
+csv_data = pd.read_csv("../../_data/StudentsPerformance.csv")
+
+graph1 = csv_data.groupby(['testPreparationCourse']).size()
 
 print(csv_data.columns)
--- case/pandas/juejin_8_1.py
+++ case/pandas/juejin_8_1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df_stockload = pd.read_csv("/Users/zifang/workplace/pycharm_workplace/py_ex_all/_data/juejin_8_1.csv")
+df_stockload = pd.read_csv("../../_data/juejin_8_1.csv")
 df_stockload.set_index(["Date"], inplace=True)
 
 '''
